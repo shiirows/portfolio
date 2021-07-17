b2aa2d6a39c4df2185c7fa909d4364d7afd80e14
Alexandre Mazoyer <mazoyeralexandre@hotmail.fr>
2021-07-17 18:12:33 +0200

    correction plus ajout html css de la page contact

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -12,17 +12,17 @@ import { QuisuisjeComponent } from './quisuisje/quisuisje.component';
 
 
 export const route :Routes = [
-{path: 'accueil', component:AccueilComponent,data: {animation: 'accueilPage'} },
-{path: 'formations', component:FormationsComponent,data: {animation: 'formPage'} },
+{path: 'accueil', component:AccueilComponent,data: {animation:'accueilPAge'} },
+{path: 'formations', component:FormationsComponent,data: {animation: 'formationsPage'} },
 {path: 'loisir', component:LoisirComponent,data: {animation: 'loisirPage'} },
 {path: 'contact', component:ContactComponent,data: {animation: 'contactPage'} },
 {path: 'bma', component:BmaComponent,data: {animation: 'bmaPage'} },
 {path: 'wild', component:WildComponent,data: {animation: 'wildPage'} },
-{path: 'jeux', component:JeuxComponent,data: {animation: 'jeux'} },
-{path: 'projetg', component:ProjetgComponent,data: {animation: 'projetg'} },
-{path: 'quisuisje', component:QuisuisjeComponent,data: {animation: 'projetg'} },
+{path: 'jeux', component:JeuxComponent,data: {animation: 'jeuxPage'} },
+{path: 'projetg', component:ProjetgComponent,data: {animation: 'projetgPage'} },
+{path: 'quisuisje', component:QuisuisjeComponent,data: {animation: 'quisuisjePage'} },
 {path: '', component:AccueilComponent },
-{path: '**', component:ConstructionComponent,data: {animation: 'consPage'} },
+{path: '**', component:ConstructionComponent,},
 
 ];
 
